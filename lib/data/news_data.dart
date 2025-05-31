bc02cad88b415101ab628224e22bc29f95e0e7bd
ayuarista <aristaadewi8@gmail.com>
2025-05-31 23:54:48 +0800

    add more data

--- lib/data/news_data.dart
+++ lib/data/news_data.dart
@@ -39,4 +39,24 @@ final List<Map<String, dynamic>> newsData = [
     'content':
         "Flights were temporarily disrupted at Liberia main airport on Thursday night after a private jet carrying President Joseph Boakai almost crashed while landing. Part of the presidential jet's landing gear malfunctioned while approaching the runway, causing a rough landing, airport authorities said. The incident, which sparked panic at the airport, forced the cancellation of all scheduled flights for the night, local media reported. President Boakai, who was returning from a trip in Nigeria with his entourage, was safely evacuated unharmed, as authorities announced an investigation.",
   },
+  {
+    'id': 5,
+    'title': 'An Illinois Town Fights to Save its Power Plant',
+    'image': 'https://images.unsplash.com/photo-1628354234455-482e157281e3?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'author': 'Taylor Swift',
+    'date': '20 Nov, 2022',
+    'category': 'Magazine',
+    'content':
+        "Flights were temporarily disrupted at Liberia main airport on Thursday night after a private jet carrying President Joseph Boakai almost crashed while landing. Part of the presidential jet's landing gear malfunctioned while approaching the runway, causing a rough landing, airport authorities said. The incident, which sparked panic at the airport, forced the cancellation of all scheduled flights for the night, local media reported. President Boakai, who was returning from a trip in Nigeria with his entourage, was safely evacuated unharmed, as authorities announced an investigation.",
+  },
+  {
+    'id': 6,
+    'title': 'An Illinois Town Fights to Save its Power Plant',
+    'image': 'https://images.unsplash.com/photo-1628354234455-482e157281e3?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'author': 'Taylor Swift',
+    'date': '20 Nov, 2022',
+    'category': 'Magazine',
+    'content':
+        "Flights were temporarily disrupted at Liberia main airport on Thursday night after a private jet carrying President Joseph Boakai almost crashed while landing. Part of the presidential jet's landing gear malfunctioned while approaching the runway, causing a rough landing, airport authorities said. The incident, which sparked panic at the airport, forced the cancellation of all scheduled flights for the night, local media reported. President Boakai, who was returning from a trip in Nigeria with his entourage, was safely evacuated unharmed, as authorities announced an investigation.",
+  },
 ];
